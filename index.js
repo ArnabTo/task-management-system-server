1781b466bf8921b30dc45618d42aa56c9629e838
ArnabTo <arnabs929@gmail.com>
2023-12-22 19:31:16 +0600

    task delete api creted

--- index.js
+++ index.js
@@ -17,34 +17,71 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 ConnectDB()
-.then(()=>{
+    .then(() => {
 
-    app.post('/tasks', async(req,res)=>{
-        try{
-            const task = req.body;
-            const createTask = new Task(task);
-            const addTaskinDB = await createTask.save();
-            res.send('succeed')
-        }catch(err){
+        app.post('/tasks', async (req, res) => {
+            try {
+                const task = req.body;
+                const createTask = new Task(task);
+                const addTaskinDB = await createTask.save();
+                res.send('succeed')
+            } catch (err) {
+                res.send(err)
+            }
+        })
+
+        app.get('/tasks', async (req, res) => {
+            const allTask = await Task.find();
+            res.send(allTask)
+        })
+        app.get('/usertasks/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email : email}
+            const result = await Task.find(query);
+            res.send(result)
+        })
+        app.delete('/deletetask/:id', async(req,res)=>{
+           const id = req.params.id;
+           const deleteTask = await Task.findByIdAndDelete(id);
+           res.send({message:'succeed'})
+        })
+
+        app.get('/todotask', async (req, res) => {
+            const query = { status: 'todo' }
+            const result = await Task.find(query)
+            res.send(result);
+        })
+        app.get('/finishedtask', async (req, res) => {
+            const query = { status: 'finished' }
+            const result = await Task.find(query)
+            res.send(result);
+        })
+
+        app.patch('/taskupdate/:id', async (req, res) => {
+          try{
+            const taskId = req.params.id;
+            console.log(taskId)  
+            const updatedTask = await Task.findOneAndUpdate(
+                {_id: taskId},
+                {status: 'finished'},
+                {upsert: true, new: true}
+            )
+            res.send({message:'succeed'})
+          }
+          catch(err){
             res.send(err)
-        }
-    })
+          }
+        })
+        app.get('/users', async (req, res) => {
+            const allUser = await User.find();
+            res.send(allUser)
+        })
 
-    app.get('/tasks', async(req,res)=>{
-        const allTask = await Task.find();
-        res.send(allTask)
+        app.get('/', (req, res) => {
+            res.send('Server is running')
+        })
+        app.listen(port, () => {
+            console.log('server is running on', port)
+        })
     })
-
-    app.get('/users', async(req,res)=>{
-        const allUser = await User.find();
-        res.send(allUser)
-    })
-
-    app.get('/', (req,res)=>{
-        res.send('Server is running')
-    })
-    app.listen(port, ()=>{
-        console.log('server is running on', port)
-    })
-})
 
--- src/model/TaskModel.js
+++ src/model/TaskModel.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    task: String
+    email: String,
+    priority: String,
+    title: String,
+    taskD: String,
+    date: String,
+    status: String
 });
 
 const Tasks = mongoose.model('Tasks', taskSchema, 'taskCollection');
